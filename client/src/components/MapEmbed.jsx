f4892f7aad67b3ec68590221e06bde4a4f520a7c
Joachim Mwengi <kimwambua96@gmail.com>
2024-01-23 00:16:33 +0300

    set-up contact page. Yet to add contact form

---
+++ client/src/components/MapEmbed.jsx
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material";
+
+const MapEmbed = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-8">
+      <div className="text-center">
+        <Typography
+          variant="h3"
+          style={{ marginBottom: "1.5rem", color: "#AC2333" }}
+        >
+          Where To Find Us
+        </Typography>
+      </div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5008.130854538936!2d36.79737912166468!3d-1.2880082065870464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1185229aaeb7%3A0x6b94799e9e7dd116!2sMosota%20Abunga%26%20Associates%20Advocates%2C%20Llp!5e0!3m2!1sen!2ske!4v1705957733909!5m2!1sen!2ske"
+        width="900"
+        height="450"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MapEmbed;
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,16 @@
+import DynamicHeader from "../components/DynamicHeader"
+import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
+import MapEmbed from "../components/MapEmbed";
+
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <>  
+    <DynamicHeader headerTitle="Contact Us" src="https://res.cloudinary.com/dfdn7sxwi/image/upload/v1705956580/still-life-with-scales-justice_2_clkcnl.jpg" />
+    <ContactForm />
+    <MapEmbed />
+    <Footer />
+    </>
   )
 }
 
