8b2d9d53899b9ec3716ba77bdaef22e0187cc8a2
Joachim-Wambua <kimwambua96@gmail.com>
2025-06-28 17:41:13 +0300

    updated dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://maassociates.co.ke/",
+  base: "/",
   // Specify the target browsers for compatibility
   // Adjust according to your specific needs
   build: {
